c93796c8d0afa3f9192b8d6d66350bd34166429d
amaboubakr85 <a.m.aboubakr85@gmail.com>
2020-07-14 21:13:38 +0300

    some new modifications

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React  from 'react';
 
 function Banner ({title,subtitle,children}){
 
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import Banner from "../components/Banner";
 import {RoomContext} from "../Context";
 import {Link} from "react-router-dom";
