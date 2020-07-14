82f6e7e4112ae3871a4d0f5fba6579592748bb16
amaboubakr85 <a.m.aboubakr85@gmail.com>
2020-07-14 21:16:06 +0300

    adding some features

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import Banner from "../components/Banner";
 import {RoomContext} from "../Context";
 import {Link} from "react-router-dom";
